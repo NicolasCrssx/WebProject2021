95f19a2393bce666ddd29951a431a02641d198db
NicolasCrssx <nicolas.cresseaux@gmail.com>
2021-10-29 20:47:16 +0200

    README pour M. Veillard

--- web-project-clocher-cresseaux/README.md
+++ web-project-clocher-cresseaux/README.md
@@ -5,3 +5,17 @@
 # Run
 
 `npm start`
+
+
+---------------------------------------------
+
+Bonjour M. Veillard, nous éspérons écrire ce message AVANT que vous fassiez la revue de notre projet. 
+
+Tout d'abord, comme vous allez pouvoir le remarquer, nous avons finalement décidé de faire notre site web 
+sur un refuge de chatons (les chats ne sont-ils pas le meilleur animal du monde ?)
+
+Notre site est en place, nous sommes actuellement en train d'essayer de faire fonctionner la base de données.
+
+Bon courage pour vos review de projets !
+
+Nicolas CRESSEAUX et Louis CLOCHER, IT3
---
+++ web-project-clocher-cresseaux/restore.sql
@@ -0,0 +1,121 @@
+--
+-- NOTE:
+--
+-- File paths need to be edited. Search for $$PATH$$ and
+-- replace it with the path to the directory containing
+-- the extracted data files.
+--
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.0
+-- Dumped by pg_dump version 13.0
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE "Project";
+--
+-- Name: Project; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE "Project" WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'French_France.1252';
+
+
+ALTER DATABASE "Project" OWNER TO postgres;
+
+\connect "Project"
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    email text,
+    password text
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, email, password) FROM stdin;
+\.
+COPY public.users (id, email, password) FROM '$$PATH$$/2985.dat';
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 4, true);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
